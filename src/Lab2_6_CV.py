edbe77f18ab8803af58aad1351553fd81255a87d
202304626 <202304626@alu.comillas.edu>
2025-03-13 15:02:11 +0100

    fin py

--- src/Lab2_6_CV.py
+++ src/Lab2_6_CV.py
@@ -1,3 +1,5 @@
+import numpy as np
+
 def cross_validation(model, X, y, nFolds):
     """
     Perform cross-validation on a given machine learning model to evaluate its performance.
@@ -45,25 +47,59 @@ def cross_validation(model, X, y, nFolds):
         nFolds = X.shape[0]
 
     # TODO: Calculate fold_size based on the number of folds
-    fold_size = None
+    fold_size = X.shape[0] // nFolds  # número de muestras entre número de folds
 
     # TODO: Initialize a list to store the accuracy values of the model for each fold
     accuracy_scores = []
 
     for i in range(nFolds):
         # TODO: Generate indices of samples for the validation set for the fold
-        valid_indices = None
+        valid_indices = np.array([j for j in range(fold_size*i, fold_size*(i+1))])
 
         # TODO: Generate indices of samples for the training set for the fold
-        train_indices = None
+        train_indices = np.array([j for j in range(X.shape[0]) if j not in valid_indices])
 
         # TODO: Split the dataset into training and validation
-        X_train, X_valid = None, None
-        y_train, y_valid = None, None
+        X_train, X_valid = X[train_indices], X[valid_indices]
+        y_train, y_valid = y[train_indices], y[valid_indices]
 
         # TODO: Train the model with the training set
+        model.fit(X_train, y_train)
 
         # TODO: Calculate the accuracy of the model with the validation set and store it in accuracy_scores
+        accuracy = model.score(X_valid, y_valid)
+
+        # Añadimos la accuracy de esta iteración a la lista
+        accuracy_scores.append(accuracy)
+
+    # Calculamos la media y la desviación típica
+    media_accuracy = np.mean(accuracy_scores)
+    std_accuracy = np.std(accuracy_scores)
 
     # TODO: Return the mean and standard deviation of the accuracy_scores
-    return None, None
+    return media_accuracy, std_accuracy
+
+
+
+
+
+
+
+#################
+#################
+#################
+#################
+#################
+#################
+#################
+#################
+#################
+#################
+#################
+#################
+#################
+#################
+#################
+#################
+#################
+######
